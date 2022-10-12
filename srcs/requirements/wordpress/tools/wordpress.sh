4cdfc1846880ab470701a173edb4034d5cb54e60
GustonMari <gustave1mary@gmail.com>
2022-10-12 18:40:31 +0200

    theme installation for wp is installed and booted

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,37 +1,26 @@
 #!/bin/sh
 
-#? a tej les variables car elles seront dispo dans le .env
-
-# MYSQL SETUP
-MYSQL_ROOT_PASSWORD="gmaryrootmode9090!"
-MYSQL_ADMIN="mastermind"
-MYSQL_ADMIN_PASSWORD="jesuis1penseur!"
-MYSQL_USER="gmary"
-MYSQL_USER_PASSWORD="jesuisgmary8080!"
-MYSQL_WP="wordpress_mysql"
-
 #? need to replace with ping interceptor
 sleep 10
+#install wp core in path
 wp core download --allow-root --locale=fr_FR --path="/var/www/html"
 
 # cd wp
 echo "\n\e[92m- 1 -\e[0m\n"
 # pwd
 # ls
-echo "saluuuuuuuttt"
 #!generate wp-config.php file with info
 wp config create --allow-root --path="/var/www/html" --dbname=${MYSQL_WP} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_USER_PASSWORD} --dbhost="mariadb" --config-file="/var/www/html/wp-config.php"
 
 # # wp db create
 echo "\n\e[92m- 2 -\e[0m\n"
 
-# sleep infinity
 # #!add information for wp
-wp core install --allow-root --path="/var/www/html" --url="gmary.42.fr" --admin_user=${MYSQL_ADMIN} --admin_password=${MYSQL_ADMIN_PASSWORD} --admin_email="gmary@student.42.fr"
+wp core install --allow-root --path="/var/www/html" --url="gmary.42.fr" --admin_user=${MYSQL_ADMIN} --admin_password=${MYSQL_ADMIN_PASSWORD} --admin_email="gmary@student.42.fr" --title="gmary.42.fr"
 echo "\n\e[92m- 3 -\e[0m\n"
 
 # #!add them for wp
-wp theme install --activate https://github.com/RoneoOrg/wp-bootstrap-starter/archive/refs/heads/master.zip
+wp theme install --allow-root --activate --path="/var/www/html" https://github.com/RoneoOrg/wp-bootstrap-starter/archive/refs/heads/master.zip 
 echo "\n\e[92m- 4 -\e[0m\n"
 
 # # wp server
@@ -39,4 +28,5 @@ mkdir -p /run/php/php7.3
 echo "\n\e[92m- 5 -\e[0m\n"
 
 # #!fast cgi process manager
-php -fpm7.3 -F
+# sleep infinity
+php-fpm7.3 -F
